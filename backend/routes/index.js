e8669359f3ad012a85ee69bf0dc6868cd99cba05
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-30 14:47:50 +0100

    removed forward slash from the candidate route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import getAllCandidates from "../models/getAllCandidates.js";
 
-router.get('/candidates', async (req, res) => {
+router.get('candidates', async (req, res) => {
     try {
        const response = await getAllCandidates()   
        res.json({ success : true, data : response})
